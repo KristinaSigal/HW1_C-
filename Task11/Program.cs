ec9b36e5d8361e1be5402523d6e8160c3219162c
kristina.sigal <kulrikh@gmail.com>
2022-08-11 13:49:44 +0300

    Add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,21 @@
+﻿// 11. Напишите программу, которая 
+// * выводит случайное трёхзначное число 
+// * удаляет вторую цифру этого числа.
+//      456 -> 46
+//      782 -> 72
+//      918 -> 98
+
+int number = new Random().Next(100, 1000); //получаем псевдослучайное число в диапазоне от 100 до 999 (+1)
+
+int deletemMidleDigit(int num)//сигнатура метода
+{
+    int firstDigit = num / 100; //нахождение первой цифры числа
+    int secondDigit = num % 10; //нахождение третьей цифры числа
+    int result = firstDigit * 10 +secondDigit;
+    return result;
+}
+
+int res = deletemMidleDigit(number);
+Console.WriteLine($"Сгенерированное трехзначное число => {number}. Обработанное двузначное число => {res}");
+
+
