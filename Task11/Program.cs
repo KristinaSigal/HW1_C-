388a0635fcd515881112e0a25b769c201db1e878
kristina.sigal <kulrikh@gmail.com>
2022-08-11 13:55:26 +0300

    Edit Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -7,15 +7,16 @@
 
 int number = new Random().Next(100, 1000); //получаем псевдослучайное число в диапазоне от 100 до 999 (+1)
 
-int deletemMidleDigit(int num)//сигнатура метода
+int DeletemMidleDigit(int num)//сигнатура метода
 {
     int firstDigit = num / 100; //нахождение первой цифры числа
     int secondDigit = num % 10; //нахождение третьей цифры числа
     int result = firstDigit * 10 +secondDigit;
+    //int result = num / 100 * 10 + num % 10;
     return result;
 }
 
-int res = deletemMidleDigit(number);
+int res = DeletemMidleDigit(number);
 Console.WriteLine($"Сгенерированное трехзначное число => {number}. Обработанное двузначное число => {res}");
 
 
