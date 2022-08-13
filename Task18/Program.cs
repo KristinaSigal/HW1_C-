771b63aa7e70757f3f6fb80b5541b2f0b2c5a6e1
kristina.sigal <kulrikh@gmail.com>
2022-08-13 15:53:35 +0300

    Add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string Span(int numb)
+{
+    if (numb == 1) return ("Значение переменных: x > 0 и у > 0");
+    if (numb == 2) return ("Значение переменных: x < 0 и у > 0");
+    if (numb == 3) return ("Значение переменных: x < 0 и у < 0");
+    if (numb == 4) return ("Значение переменных: x > 0 и у < 0");
+    return ("Указана некорректная четверть");
+}
+
+string result = Span(num);
+Console.WriteLine(result);
