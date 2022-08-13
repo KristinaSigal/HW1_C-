e4483d42cf8ceb54d68e70c33363623374d2387b
kristina.sigal <kulrikh@gmail.com>
2022-08-13 12:26:18 +0300

    Add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая 
+//      1. принимает на вход трёхзначное число и 
+//      2. на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int MidleDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = firstDigit % 10;
+    return secondDigit > 0 ? secondDigit : secondDigit * -1;
+}
+
+int res = MidleDigit(number);
+Console.WriteLine(res);
